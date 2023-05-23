5bd684d20938464bb8fe0f432a2a28cf203f3404
kawsarhamidi <kawsarhamidi77@gmail.com>
2023-05-23 07:07:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="btn-group btn-group-vertical">
+        <button className="btn btn-active">Button</button>
+        <button className="btn">Button</button>
+        <button className="btn">Button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
